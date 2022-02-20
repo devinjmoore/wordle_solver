b50414f2941666003d66c05e7859d180dc5e748a
Devin <devinjmoore21@gmail.com>
2022-02-20 09:28:28 -0800

    core functionality complete. could use a visual touch up

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,79 +1,93 @@
 from typing import List
-import sys
-
-import wordle_solver_gui as gui
-import easygui
+from wordle_answers import ALL_WORDLE_ANSWERS
 
 
 class Wordle:
     class Guess:
-        """A class to represent a an incorrect guess in a Wordle game.
-
-        Returns:
-            [type]: [description]
+        """A class to represent an incorrect guess in a Wordle game. A guess
+        object contains the word which was guessed along with a list of five
+        bools. These bools indicate whether the corresponding letter in the word
+        is
+            a. in the solution and in the correct place (True)
+            b. in the solution, but in the wrong place (False)
+            a. not in the solution at all (None)
         """
         _LETTER_NOT_IN_WORD = None
         _LETTER_CORRECT = True
-        _LETTER_IN_WRONG_PLACE = False
+        _LETTER_WRONG_PLACE = False
         
-        def __init__(self, word:str, hints: List[bool]) -> None:
+        def __init__(self, word: str, hints: List[bool]) -> None:
             self.word = word
             self.hints = hints
-            
-    def __init__(self) -> None:
-        self.guessed: List[Wordle.Guess] = []
 
-    def set_guesses(self, guesses:List[Guess]):
-        self.guesses = guesses
+    def __init__(self, guesses_made) -> None:
+        self.guesses_made: List[Wordle.Guess] = guesses_made
+        
+    def get_guesses_made(self) -> List[Guess]:
+        return self.guesses_made
 
 
 class WordleSolver:
     _LEN_GUESS = 5
-    
-    answers: List[str] = open("answers.txt").read().split('\n')
 
-    def __init__(self, wordle:Wordle) -> None:
+    def __init__(self, wordle: Wordle) -> None:
         self.wordle = wordle
-    
-    def _check_new_guess(self, new_guess:str) -> bool:
-        """Determine if a new guess follows all known hints in this Wordle
 
-        Args:
-            new_guess (str): The proposed Wordle solution
-
-        Returns:
-            bool: Indicates whether the new guess uses all of this Wordle's
-            guesses' hints
+    def _check_solution(self, solution: str) -> bool:
         """
-        for guess in self.wordle.guessed:
-            for i in range(self._LEN_GUESS):
-                if new_guess[i] == guess.word[i]:
-                    # Check if we already know that this letter shouldn't be here
-                    if (guess.hints[i] == guess._LETTER_NOT_IN_WORD or
-                        guess.hints[i] == guess._LETTER_IN_WRONG_PLACE):
-                        return False
-                else:
-                    # Check if we already know which letter should be here
-                    if guess.hints[i] == guess._LETTER_CORRECT:
-                        return False
-        
-        return True
-    
-    def get_valid_answers(self) -> List[str]:
-        """Get a list of all possible solutions to this Wordle based on all
-        known hints
+        Args:
+            solution (str): The proposed Wordle solution
 
         Returns:
-            List[str]: A list containing all valid Wordle words which adhere to
-            all hints from current guesses
+            bool: Indicates if a proposed solution follows all known hints in
+            this Wordle
+        """
+
+        # Check that the solution utilizes all the hints from previous guesses
+        for guess in self.wordle.get_guesses_made():
+            # Check letter by letter
+            for i in range(self._LEN_GUESS):
+                if guess.hints[i] == guess._LETTER_CORRECT:
+                    # Verify that the solution uses the correct letter here
+                    if solution[i] != guess.word[i]:
+                        return False
+
+                elif guess.hints[i] == guess._LETTER_WRONG_PLACE:
+                    # region Verify that the solution uses this letter
+                    letter_match_indices = [
+                        index for (index, letter) in enumerate(solution)
+                        if letter == guess.word[i]
+                    ]
+                    
+                    # Remove indices of already-correct letters
+                    for index in letter_match_indices:
+                        if guess.hints[index] == guess._LETTER_CORRECT:
+                            letter_match_indices.remove(index)
+
+                    if not len(letter_match_indices) > 0:
+                        return False
+                    # endregion
+
+                    # Verify that the solution only uses this letter elsewhere
+                    if solution[i] == guess.word[i]:
+                        return False
+
+                elif guess.hints[i] == guess._LETTER_NOT_IN_WORD:
+                    # Verify that the solution does not contain this letter
+                    if guess.word[i] in solution:
+                        return False
+
+        return True
+
+    def get_valid_answers(self) -> List[str]:
+        """
+        Returns:
+            List[str]: A list of all possible solutions to this Wordle which
+            utilize all known hints
         """
         valid_answers = []
-        for answer in self.answers:
-            if self._check_new_guess(answer):
+        for answer in ALL_WORDLE_ANSWERS:
+            if self._check_solution(answer):
                 valid_answers.append(answer)
-                
-        return valid_answers
 
-        
-if __name__ == '__main__':
-    sys.exit(gui.app.exec())
+        return valid_answers
--- wordle_solver_gui.py
+++ wordle_solver_gui.py
@@ -1,17 +1,18 @@
-from typing import List
+import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 
+from wordle_solver import Wordle, WordleSolver
+
+_COLOR_LETTER_NOT_IN_WORD = "#202020"
 _COLOR_LETTER_CORRECT = "#009600"
 _COLOR_LETTER_WRONG_PLACE = "#ffc425"
-_COLOR_LETTER_NOT_IN_WORD = "#202020"
-_COLOR_LETTER_NOT_ENTERED = "#121213"
-
 
 class LetterTile(QtWidgets.QPushButton):
+    
     _STYLE_HIGHLIGHTED = "border-color: white; border-width: 2px;"
 
     letter_clicked = QtCore.Signal(QtWidgets.QPushButton)
-    change_hint = QtCore.Signal(QtWidgets.QPushButton)
+    hint_changed = QtCore.Signal(QtWidgets.QPushButton)
 
     def __init__(self, parent) -> None:
         super().__init__(parent=parent)
@@ -25,18 +26,25 @@ class LetterTile(QtWidgets.QPushButton):
         stylesheet[value_index] = value + ';'
         self.setStyleSheet(' '.join(stylesheet))
 
-    def change_background_color(self, color: str):
+    def change_bg_color(self, color: str):
         self._change_style_property("background-color", color)
 
-    def clear(self) -> None:
-        self.setText("")
-        self.change_background_color()
-
+    def hint(self) -> bool:
+        stylesheet = self.styleSheet().split()
+        
+        letter_color = stylesheet[stylesheet.index("background-color:") + 1].strip(';')
+        if letter_color == _COLOR_LETTER_CORRECT:
+            return Wordle.Guess._LETTER_CORRECT
+        elif letter_color == _COLOR_LETTER_WRONG_PLACE:
+            return Wordle.Guess._LETTER_WRONG_PLACE
+        else:
+            return Wordle.Guess._LETTER_NOT_IN_WORD
+        
     def is_empty(self) -> bool:
         return self.text() == ''
 
     def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
-        self.change_hint.emit(self)
+        self.hint_changed.emit(self)
         self.letter_clicked.emit(self)
 
     def setHighlight(self, active: bool) -> None:
@@ -46,7 +54,6 @@ class LetterTile(QtWidgets.QPushButton):
             self.setStyleSheet(
                 self.styleSheet().replace(self._STYLE_HIGHLIGHTED, "")
             )
-        pass
 
 
 class WordleSolverGUI(QtWidgets.QWidget):
@@ -55,12 +62,14 @@ class WordleSolverGUI(QtWidgets.QWidget):
     _LETTER_WIDTH = 100
     _LETTER_HEIGHT = 100
 
-    _LETTER_LINE_LENGTH = 5
-    _NUM_LETTER_LINES = 5
+    _LENGTH_ROW = 5
+    _NUM_ROWS = 5
+    _COLOR_LETTER_NOT_ENTERED = "#121213"
 
+    _RESULTS_VIEWER_GEOMETRY = QtCore.QRect(0, 0, _APP_WIDTH, _APP_WIDTH)
     _SUBMIT_BUTTON_GEOMETRY = QtCore.QRect(
         _APP_WIDTH / 2 - (3 * _LETTER_WIDTH / 2),
-        _NUM_LETTER_LINES * _LETTER_HEIGHT + _LETTER_HEIGHT / 4,
+        _NUM_ROWS * _LETTER_HEIGHT + _LETTER_HEIGHT / 4,
         3 * _LETTER_WIDTH,
         _LETTER_HEIGHT / 2
     )
@@ -69,22 +78,24 @@ class WordleSolverGUI(QtWidgets.QWidget):
         super().__init__()
 
         # region Initialize widget layout
-        self.setStyleSheet(open("stylesheet.txt").read())
+        self.setStyleSheet(
+            "QPushButton {background-color: #101010; font: 48pt \"Arial\";"
+            "border-style: solid; border-width: 3px; border-color: black;}"
+        )
         self.setGeometry(500, 300, self._APP_WIDTH, self._APP_HEIGHT)
         # endregion
 
         # region Initialize letter grid layout
-        self.letter_lines = [
-            [
-                LetterTile(self) for j in range(self._LETTER_LINE_LENGTH)
-            ] for i in range(self._NUM_LETTER_LINES)
+        self.letter_grid = [
+            [LetterTile(self) for j in range(self._LENGTH_ROW)]
+            for i in range(self._NUM_ROWS)
         ]
         # endregion
 
         # region Configure each letter tile
-        for i in range(len(self.letter_lines)):
-            for j in range(len(self.letter_lines[i])):
-                letter = self.letter_lines[i][j]
+        for i in range(self._NUM_ROWS):
+            for j in range(self._LENGTH_ROW):
+                letter = self.letter_grid[i][j]
                 letter.setGeometry(
                     self._LETTER_WIDTH * j,
                     self._LETTER_HEIGHT * i,
@@ -94,24 +105,33 @@ class WordleSolverGUI(QtWidgets.QWidget):
                 letter.line_number = i
                 letter.letter_index = j
 
-                letter.setStyleSheet(f"background-color: {_COLOR_LETTER_NOT_ENTERED};")
+                letter.setStyleSheet(
+                    f"background-color: {self._COLOR_LETTER_NOT_ENTERED};"
+                )
 
                 letter.letter_clicked.connect(self._move_cursor_to_letter)
-                letter.change_hint.connect(self._cycle_through_hints)
+                letter.hint_changed.connect(self._cycle_through_hints)
+        # endregion
+        
+        # region Initialize results viewer
+        self.results = QtWidgets.QPlainTextEdit(self)
+        self.results.setGeometry(self._RESULTS_VIEWER_GEOMETRY)
+        self.results.setReadOnly(True)
+        self.results.hide()
+        self.results_showing = False
         # endregion
 
         # region Initialize submit button
-        self.submit_button = QtWidgets.QPushButton(
-            text="Submit Guesses",
-            parent=self
-        )
+        self.submit_button = QtWidgets.QPushButton(self)
+        self.submit_button.setText("Get Solutions")
         self.submit_button.setGeometry(self._SUBMIT_BUTTON_GEOMETRY)
         self.submit_button.setStyleSheet("font: 24pt Arial; color: white;")
+        self.submit_button.clicked.connect(self._submit_guesses)
         # endregion
 
-        # region Initialize the user cursor
+        # region Initialize the user's grid cursor
         self.letter_cursor = [0, 0]
-        self.current_letter().setHighlight(True)
+        self._current_letter().setHighlight(True)
         # endregion
         
         self.show()
@@ -120,26 +140,25 @@ class WordleSolverGUI(QtWidgets.QWidget):
     def _cycle_through_hints(self, letter:LetterTile) -> None:
         if letter.is_empty():
             return
+        
         letter_style = letter.styleSheet()
         if _COLOR_LETTER_CORRECT in letter_style:
-            letter.change_background_color(_COLOR_LETTER_WRONG_PLACE)
+            letter.change_bg_color(_COLOR_LETTER_WRONG_PLACE)
         elif _COLOR_LETTER_WRONG_PLACE in letter_style:
-            letter.change_background_color(_COLOR_LETTER_NOT_IN_WORD)
+            letter.change_bg_color(_COLOR_LETTER_NOT_IN_WORD)
         else:
-            letter.change_background_color(_COLOR_LETTER_CORRECT)
+            letter.change_bg_color(_COLOR_LETTER_CORRECT)
 
     def _delete_current_letter(self) -> None:
-        self.current_letter().setText('')
-        self.current_letter().change_background_color(
-            _COLOR_LETTER_NOT_ENTERED
-        )
+        self._current_letter().setText('')
+        self._current_letter().change_bg_color(self._COLOR_LETTER_NOT_ENTERED)
 
 
     def _move_cursor(move_cursor_in_a_direction):
         def wrapper(*args, **kwargs):
-            gui: WordleSolverGUI = args[0]
+            self: WordleSolverGUI = args[0]
 
-            gui.current_letter().setHighlight(False)
+            self._current_letter().setHighlight(False)
 
             if len(args) == 1:
                 move_cursor_in_a_direction(gui)
@@ -147,7 +166,7 @@ class WordleSolverGUI(QtWidgets.QWidget):
                 letter: LetterTile = args[1]
                 move_cursor_in_a_direction(gui, letter)
 
-            gui.current_letter().setHighlight(True)
+            gui._current_letter().setHighlight(True)
         return wrapper
 
     @_move_cursor
@@ -157,7 +176,7 @@ class WordleSolverGUI(QtWidgets.QWidget):
 
     @_move_cursor
     def _move_cursor_down(self) -> None:
-        if self.letter_cursor[0] < len(self.letter_lines) - 1:
+        if self.letter_cursor[0] < len(self.letter_grid) - 1:
             self.letter_cursor[0] += 1
 
     @_move_cursor
@@ -167,15 +186,15 @@ class WordleSolverGUI(QtWidgets.QWidget):
         if self.letter_cursor[1] > 0:
             self.letter_cursor[1] -= 1
         else:
-            current_line = self.letter_lines[self.letter_cursor[0]]
+            current_line = self.letter_grid[self.letter_cursor[0]]
             self.letter_cursor[1] = len(current_line) - 1
             self._move_cursor_up()
 
     @_move_cursor
     def _move_cursor_right(self) -> None:
-        current_line = self.letter_lines[self.letter_cursor[0]]
+        current_line = self.letter_grid[self.letter_cursor[0]]
         if self.letter_cursor == [
-            len(self.letter_lines) - 1,
+            len(self.letter_grid) - 1,
             len(current_line) - 1
         ]:
             return
@@ -190,21 +209,42 @@ class WordleSolverGUI(QtWidgets.QWidget):
     def _move_cursor_to_letter(self, letter: LetterTile) -> None:
         self.letter_cursor = [letter.line_number, letter.letter_index]
 
+    def _submit_guesses(self):
+        # Create a list of Wordle Guesses for the Wordle object
+        guesses = []
+        for row in self.letter_grid:
+            if row[0].is_empty():
+                break
+            word = ''.join([letter.text() for letter in row])
+            hints = [letter.hint() for letter in row]
+            guesses.append(Wordle.Guess(word, hints))
+            
+        wordle = Wordle(guesses)
+        wordle_solver = WordleSolver(wordle)
+        solutions = wordle_solver.get_valid_answers()
+        
+        self.results.setPlainText("Possible Solutions: " + repr(solutions))
+        self.results.show()
+        
+        self.results_showing = True
 
-    def current_letter(self) -> LetterTile:
-        return self.letter_lines[self.letter_cursor[0]][self.letter_cursor[1]]
+    def _current_letter(self) -> LetterTile:
+        return self.letter_grid[self.letter_cursor[0]][self.letter_cursor[1]]
 
     def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        if self.results_showing:
+            self.results.hide()
+            self.results_showing = False
+            return
+        
         key = event.key()
         if event.text().isalpha():
-            if self.current_letter().is_empty():
-                self.current_letter().change_background_color(
-                    _COLOR_LETTER_NOT_IN_WORD
-                )
-            self.current_letter().setText(event.text().upper())
+            if self._current_letter().is_empty():
+                self._current_letter().change_bg_color(_COLOR_LETTER_NOT_IN_WORD)
+            self._current_letter().setText(event.text().upper())
             self._move_cursor_right()
         elif key == QtCore.Qt.Key_Backspace:
-            if self.current_letter().is_empty():
+            if self._current_letter().is_empty():
                 self._move_cursor_left()
                 self._delete_current_letter()
             else:
@@ -219,16 +259,12 @@ class WordleSolverGUI(QtWidgets.QWidget):
         elif key == QtCore.Qt.Key_Right:
             self._move_cursor_right()
         elif key == QtCore.Qt.Key_Space:
-            self.current_letter().change_hint.emit(self.current_letter())
-        elif key == QtCore.Qt.Key_Enter:
-            self.submit_button.clicked.emit()
+            self._current_letter().hint_changed.emit(self._current_letter())
+        elif key in [QtCore.Qt.Key_Enter, QtCore.Qt.Key_Return]:
+            self._submit_guesses()
 
-    def letter_grid(self) -> List[List[str]]:
-        return [
-            [
-                letter.text() for letter in self.letter_lines[i]
-            ] for i in range(self._NUM_LETTER_LINES)
-        ]
 
-app = QtWidgets.QApplication([])
-gui = WordleSolverGUI()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    gui = WordleSolverGUI()
+    sys.exit(app.exec())
